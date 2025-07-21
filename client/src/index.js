f68b780cdee47fcebc7d12919ad33cf099561c3d
Shuvo Kumer Ray <sk.ray1998t@gmail.com>
2025-07-21 18:09:20 +0600

    Client added

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+// client/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css'; // <--- UPDATED PATH HERE
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
